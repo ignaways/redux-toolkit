aeb13cab2aa0774ac64249ab31528672c0668786
unknown <ignatius.pratama@inticorp.tech>
2022-08-25 21:51:03 +0700

    redux toolkit

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+import {decrement, increment} from './redux/features/counter'
 
 function App() {
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>total : {count}</div>
+      <div>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
         >
-          Learn React
-        </a>
-      </header>
+          Decrement
+        </button>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+      </div>
     </div>
   );
 }
